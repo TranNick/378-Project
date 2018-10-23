940a7786a8d94bcd1b6f4ec37283687e21b2f84a
trannick <nicktran29@gmail.com>
2018-10-23 11:46:03 -0700

    added hmac

--- enc.py
+++ enc.py
@@ -2,68 +2,103 @@ import os
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import padding
+from cryptography.hazmat.primitives import hashes, hmac
 
-backend=default_backend()
+#backend=default_backend()
 message=b"a secret message for padding "
-key=os.urandom(32)
-iv=os.urandom(16)
 path='/home/nick/Downloads/378/text.txt'
 
-def myEncryptor(text):
-    padder=padding.PKCS7(128).padder()
-    paddedText=padder.update(text) 
-    paddedText+=padder.finalize()
-    cipher=Cipher(algorithms.AES(key), modes.CBC(iv), backend=backend)
-    encrypt=cipher.encryptor()
-    cipherText=encrypt.update(paddedText) + encrypt.finalize()
-    print("Padded text: ", paddedText)
-    return cipherText
+def myEncrypt(key, text, hmacKey):
+    if len(key)<32:
+        print("The key must be 32 bytes")
+        return "Error"
+    else:
+        iv=os.urandom(16)
 
-def myDecryptor(cipherText):
-    cipher=Cipher(algorithms.AES(key), modes.CBC(iv), backend=backend)
+        padder=padding.PKCS7(128).padder()
+        paddedText=padder.update(text) 
+        paddedText+=padder.finalize()
+
+        cipher=Cipher(algorithms.AES(key), modes.CBC(iv), backend=default_backend())
+        encrypt=cipher.encryptor()
+        cipherText=encrypt.update(paddedText) + encrypt.finalize()
+
+        h=hmac.HMAC(hmacKey, hashes.SHA256(), backend=default_backend())
+        h.update(cipherText)
+
+        return (cipherText, iv, h.finalize())
+
+def myDecrypt(key, iv, cipherText, hmacKey, h):
+    hTag=hmac.HMAC(hmacKey, hashes.SHA256(), backend=default_backend())
+    hTag.update(cipherText)
+    hTag.verify(h)
+
+    cipher=Cipher(algorithms.AES(key), modes.CBC(iv), backend=default_backend())
     decrypt=cipher.decryptor()
     cipherText=decrypt.update(cipherText) + decrypt.finalize()
+
     unpadder=padding.PKCS7(128).unpadder()
     unpaddedText=unpadder.update(cipherText) + unpadder.finalize()
+
     return unpaddedText
 
-def myFileEncryptor():
-    with open(path, 'r') as file:
+def myFileEncrypt(path, hmacKey):
+    with open(path, 'rb') as file:
         fileText=file.read()
     print("\nOriginal Text: ", fileText)
-    fileTextBytes=fileText.encode()
-    fileCipher=myEncryptor(fileTextBytes)
+
+    key=os.urandom(32)
+    ext=os.path.splitext(path)[1]
+    (fileCipher, iv, h)=myEncrypt(key, fileText, hmacKey)
     returnCipher=fileCipher
+    h=hmac.HMAC(hmacKey, hashes.SHA256(), backend=default_backend())
+    h.update(returnCipher)
+
     print("\nWriting encrypted text to file...")
     wr=open(path, 'wb')
-    #wr=open('/home/nick/Downloads/378/cipher.txt', 'wb')
     wr.write(fileCipher)
     wr.close()
-    return (returnCipher)
 
-def myFileDecryptor():
+    return (returnCipher, iv, key, ext, h.finalize())
+
+def myFileDecrypt(path, cipherText, iv, key, hmacKey, h):
+    hTag=hmac.HMAC(hmacKey, hashes.SHA256(), backend=default_backend())
+    hTag.update(cipherText)
+    hTag.verify(h)
+
     with open(path, 'rb') as file:
        fileText=file.read()
-    # with open('/home/nick/Downloads/378/cipher.txt', 'rb') as file:
-    #     fileText=file.read()
     print("\nReading Encrypted Text from File: ", fileText)
-    filePlain=myDecryptor(fileText)
+
+    filePlain=myDecrypt(key, iv, cipherText, hmacKey, h)
     wr=open(path, 'w')
     wr.write(filePlain.decode())
     wr.close()   
+
     return (filePlain)
         
 def main():
     print("Original message: ", message)
-    print("\nNow encrypting...\n")
-    cipherText=myEncryptor(message)
-    print("Ciphertext: ", cipherText)
-    print("\nNow decrypting...\n")
-    myDecryptor(cipherText)
-    print("Plaintext: ", myDecryptor(cipherText))
-    print("\nEncrypting file contents...")
-    myFileEncryptor()
-    print("\nDecrypted file contents: ", myFileDecryptor())
+    # print("\nNow encrypting...\n")
+    # cipherText=myEncryptor(message)
+    # print("Ciphertext: ", cipherText)
+    # print("\nNow decrypting...\n")
+    # myDecryptor(cipherText)
+    # print("Plaintext: ", myDecryptor(cipherText))
+    # print("\nEncrypting file contents...")
+    # myFileEncryptor()
+    # print("\nDecrypted file contents: ", myFileDecryptor())
+    key=os.urandom(32)
+    hmacKey=os.urandom(32)
+    (C, IV, h)=myEncrypt(key, message, hmacKey)
+    print(C)
+   # print(h)
+    ogMessage=myDecrypt(key, IV, C, hmacKey, h)
+    (C, IV, key, ext, hFile)=myFileEncrypt(path, hmacKey)
+    print("CipherText: {}\nIV: {}\nkey: {}\nextension: {}\n" .format(C, IV, key, ext))
+    fileText=myFileDecrypt(path, C, IV, key, hmacKey, hFile)
+    print(fileText)
+
 
 main()
 
