5b41189cea39829ff2ddde207e41c3dbdc9300d3
trannick <nicktran29@gmail.com>
2018-12-06 09:25:36 -0800

    added rsa walk

--- rsa.py
+++ rsa.py
@@ -1,5 +1,6 @@
 import os 
 import json
+import base64
 
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 from cryptography.hazmat.backends import default_backend
@@ -9,13 +10,15 @@ from cryptography.hazmat.primitives import hashes, hmac
 from cryptography.hazmat.primitives.asymmetric import rsa
 from cryptography.hazmat.primitives import serialization
 
-path='/home/nick/Downloads/378/text.txt'
+testpath='/home/nick/Downloads/378/text.txt'
+#directory = '/home/nick/Downloads/378/asdf'
+projectPath = '/home/nick/Downloads/378/Project378'
 
 def RSAKeyGen():
     pemFiles=[]
     filePath=os.getcwd()
     for files in os.listdir(filePath):
-        if files.endswith(".pem"):
+        if files.lower().endswith(".pem"):
             pemFiles.append(files)
 
     if len(pemFiles) > 0:
@@ -158,6 +161,7 @@ def MyRSAEncrypt(filePath, publicKeyPath):
         )
     )
     return (RSACipher, c, iv, tag, ext)
+
 def MyRSADecrypt(RSACipher, C, IV, tag, ext, RSA_Privatekey_path, filePath):
     with open(RSA_Privatekey_path, 'rb') as file:
         privKey=serialization.load_pem_private_key(
@@ -177,10 +181,72 @@ def MyRSADecrypt(RSACipher, C, IV, tag, ext, RSA_Privatekey_path, filePath):
     
     return plainText
 
+def EncryptAll(directory, publicPath):
+    for root, dirs, filesToEncrypt in os.walk(directory):
+        for files in filesToEncrypt:
+            if "rsa.py" not in files and "public.pem" not in files and "private.pem" not in files  and not files.endswith(".json") and not files.endswith(".git"):       
+                fileDirectory = root + "/" + files
+                (RSACipher, c, iv, tag, ext)=MyRSAEncrypt(fileDirectory, publicPath)
+
+                fileName=os.path.splitext(fileDirectory)[0] +".json"
+
+               # btsCiphertext = base64.encodebytes(c)
+                encodedCiphertext = base64.encodebytes(c).decode('ascii')
+               # btsEncryptedKeys = base64.encodebytes(RSACipher)
+                encodedEncryptedKeys = base64.encodebytes(RSACipher).decode('ascii')
+                #btsHMACTag = base64.encodebytes(tag)
+                encodedHMACTag = base64.encodebytes(tag).decode('ascii')
+                #btsIV = base64.encodebytes(iv)
+                encodedIV = base64.encodebytes(iv).decode('ascii')
+
+                jsonObj={"RSA Cipher: " : encodedEncryptedKeys, "Ciphertext: " : encodedCiphertext, "IV: " : encodedIV, "Tag: " : encodedHMACTag, "Extension: " : ext}
+                
+                jsonFile=json.dumps(jsonObj)
+                jsonOutput=open(fileName, 'w')
+                jsonOutput.write(jsonFile)
+                os.remove(directory+"/"+files)
+
+def DecryptAll(directory, privatePath):
+    #os.chdir(directory)
+    for root, dirs, filesToDecrypt in os.walk(directory):
+        for files in filesToDecrypt:
+            if files.endswith(".json"):
+                jsonInput = open(files, "r")
+                jsonData = jsonInput.read()
+
+                jsonObj = json.loads(jsonData)
+
+                #stbCiphertext = jsonObj["Ciphertext: "].encode('ascii')
+                decodedCiphertext = base64.decodebytes(stbCiphertext)
+                #stbEncryptedKeys = jsonObj["RSA Cipher: "].encode('ascii')
+                decodedEncryptedKeys = base64.decodebytes(stbEncryptedKeys)
+                #stbHMACTag = jsonObj["Tag: "].encode('ascii')
+                decodedHMACTag = base64.decodebytes(stbHMACTag)
+                #stbIV = jsonObj["IV: "].encode('ascii')
+                decodedIV = base64.decodebytes(stbIV)
+                ext = jsonObj["Extension: "]
+
+                plaintext = MyRSADecrypt(decodedEncryptedKeys, decodedCiphertext, decodedIV, decodedHMACTag, ext, privatePath, files)
+                filePath=os.path.splitext(root+"/"+files)[0] + ext
+                fileOut=open(filePath, 'wb')
+                fileOut.write(plaintext)
+                os.remove(files)
+        # print(os.path.dirname(directory))
+        # print(directory)
+        # print(files)
+    #print("hello")
+
 def main():
     (privatePath, publicPath) = RSAKeyGen()
     print("private path ", privatePath)
     print("public path", publicPath)
-    (RSACipher, C, IV, tag, ext) = MyRSAEncrypt(path, publicPath)
-    text=MyRSADecrypt(RSACipher, C, IV, tag, ext, privatePath, path)
+    # (RSACipher, C, IV, tag, ext) = MyRSAEncrypt(path, publicPath)
+    # text=MyRSADecrypt(RSACipher, C, IV, tag, ext, privatePath, path)
+    path1=os.getcwd()
+    print(path1)
+    # EncryptAll(path1, publicPath)
+    # x = input("Enter something to continue: ")
+    # DecryptAll(path1, privatePath)
+
+    
 main()
